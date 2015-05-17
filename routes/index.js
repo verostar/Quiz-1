ecf4127221f848adaf36edb03d680492d4af7296
jgdiaz <javi.gdiaz93@gmail.com>
2015-05-17 19:09:47 +0200

    HTTPS seguro

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,7 @@ router.get('/author', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // autoload :quizId
+router.param('commentId', commentController.load); // autoload :commentId
 
 // Definición de rutas de sesión
 router.get('/login', sessionController.new);	//Formulario Login
@@ -36,5 +37,6 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 // Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
